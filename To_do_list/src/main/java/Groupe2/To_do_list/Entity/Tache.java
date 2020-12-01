5ae7aed8c20231b1a228c2b891ced7295f3dfeb1
Hector <hector.dasse@orange.fr>
2020-12-01 14:41:57 +0100

    entity tache

--- To_do_list/src/main/java/Groupe2/To_do_list/Entity/Tache.java
+++ To_do_list/src/main/java/Groupe2/To_do_list/Entity/Tache.java
@@ -1,8 +1,34 @@
 package Groupe2.To_do_list.Entity;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Tache {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer Id_Tache;
+
+    private String Titre;
+    private String Texte;
+    private Date Date_creation;
+    private Date Date_modification;
+
+    /*@OneToOne
+    private Personne personne;
+
+    @OneToOne
+    private Personne personne_creation;
+
+    @OneToOne
+    private Personne personne_Modification;
+
+    @OneToOne
+    private Status status;
+    */
+
+
+
 
 }
