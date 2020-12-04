da495c42312e8c3e6b3599b8a26ca2efc94ee5ae
Hector <hector.dasse@orange.fr>
2020-12-04 09:43:10 +0100

    page ajout tache

--- src/main/java/groupe2/to_do_list/controller/TacheController.java
+++ src/main/java/groupe2/to_do_list/controller/TacheController.java
@@ -7,15 +7,16 @@ import groupe2.to_do_list.entity.Status;
 import groupe2.to_do_list.repository.TacheRepository;
 import groupe2.to_do_list.repository.PersonneRepository;
 import groupe2.to_do_list.repository.StatusRepository;
+import groupe2.to_do_list.service.PersonneService;
 import groupe2.to_do_list.service.TacheService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Date;
 import java.util.List;
@@ -89,4 +90,69 @@ public class TacheController {
 	    	return "Unhandled error : " + e;
 	    }
     }
+
+
+	// Show Register page.
+	@RequestMapping(value = "/addtache", method = RequestMethod.GET)
+	public String addpersonne(Model model) {
+
+		Tache tache = new Tache();
+		List<Status> statuses = statusRepository.findAll();
+		List<Personne> personnes = personneRepository.findAll();
+		model.addAttribute("Statuses", statuses);
+		model.addAttribute("Personnes", personnes);
+		model.addAttribute("title", "Ajouter une tache");
+		model.addAttribute("tache", tache);
+
+		return "addtache";
+	}
+
+	@RequestMapping(value = "/updatetache", method = RequestMethod.GET)
+	public String updatepersonne(@RequestParam int id, Model model) {
+
+		Optional<Tache> optionalTache = tacheRepository.findById(id);
+		if (optionalTache.isPresent()){
+			Tache tache = optionalTache.get();
+			List<Status> status = statusRepository.findAll();
+			List<Personne> personnes = personneRepository.findAll();
+			model.addAttribute("Statuses", status);
+			model.addAttribute("Personnes", personnes);
+			model.addAttribute("title", "Ajouter une tache");
+			model.addAttribute("tache", tache);
+
+			return "addtache";
+
+		}else {
+			return "Error";
+		}
+	}
+
+	// This method is called to save the registration information.
+	// @Validated: To ensure that this Form
+	// has been Validated before this method is invoked.
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String saveRegister(Model model, //
+							   @ModelAttribute("tache") @Validated Tache tache, //
+							   BindingResult result, //
+							   final RedirectAttributes redirectAttributes) {
+
+		// Validate result
+		if (result.hasErrors()) {
+			return "addtache";
+		}
+		try {
+			if (tache.getId_Tache() == null){
+
+			} else {
+				//update
+			}
+		}
+		// Other error!!
+		catch (Exception e) {
+			System.out.println("error");
+			model.addAttribute("errorMessage", "Error: " + e.getMessage());
+			return "addpersonne";
+		}
+		return "redirect:/";
+	}
 }
